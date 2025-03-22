6316b68abddb8ac53e96674202f4bb8b7231be0e
Jeff Reiner <theundeadghost@gmail.com>
2025-03-22 17:03:49 +0100

    Adding in Bluesky link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,2 +1,4 @@
 export const REGISTER_LINK =
   "https://cryptpad.fr/form/#/2/form/view/BOJlNHmbJnP3jWa5TvOCd7qHvKtMcmCp5Ri6z4GiIgQ/";
+
+export const BLUESKY_LINK = "https://bsky.app/profile/swapfest.berlin";
