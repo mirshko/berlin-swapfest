fb42daf30a4d2e07e4a7ebddb1c241515f8e4edd
Jeff Reiner <theundeadghost@gmail.com>
2025-04-12 21:12:53 +0200

    Add contact link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,3 +2,5 @@ export const REGISTER_LINK =
   "https://cryptpad.fr/form/#/2/form/view/BOJlNHmbJnP3jWa5TvOCd7qHvKtMcmCp5Ri6z4GiIgQ/";
 
 export const BLUESKY_LINK = "https://bsky.app/profile/swapfest.berlin";
+
+export const EMAIL_LINK = "mailto:swapfest.berlin@proton.me";
