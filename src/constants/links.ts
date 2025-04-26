e069f29dba122ac7bb932f8dc1ef74ae932e9d31
Jeff Reiner <theundeadghost@gmail.com>
2025-04-26 23:27:47 +0200

    Adding table register links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,8 @@
 export const REGISTER_LINK =
-  "https://cryptpad.fr/form/#/2/form/view/BOJlNHmbJnP3jWa5TvOCd7qHvKtMcmCp5Ri6z4GiIgQ/";
+  "https://cryptpad.fr/form/#/2/form/view/sH5bEqyLMF9pUJp5autvBgrRWvNWdZ+W5WX85aywHE0/";
+
+export const TABLE_REGISTER_LINK =
+  "https://cryptpad.fr/form/#/2/form/view/givYlhPeujRAMIdzVEXh99kslfEN15l9ejiJ0+2ERHQ/";
 
 export const BLUESKY_LINK = "https://bsky.app/profile/swapfest.berlin";
 
