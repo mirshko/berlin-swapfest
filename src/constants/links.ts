53101eaa980b7198cc33e61b72e4277b9cc95183
Jeff Reiner <theundeadghost@gmail.com>
2025-05-17 18:57:27 +0200

    Rename link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,7 +1,7 @@
 export const REGISTER_LINK =
   "https://cryptpad.fr/form/#/2/form/view/sH5bEqyLMF9pUJp5autvBgrRWvNWdZ+W5WX85aywHE0/";
 
-export const TABLE_REGISTER_LINK =
+export const REGISTER_TABLE_LINK =
   "https://cryptpad.fr/form/#/2/form/view/givYlhPeujRAMIdzVEXh99kslfEN15l9ejiJ0+2ERHQ/";
 
 export const BLUESKY_LINK = "https://bsky.app/profile/swapfest.berlin";
