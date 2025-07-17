da3d99b8ba0b9032fcaa3deacc7cf3ebcd8a1d06
Jeff Reiner <theundeadghost@gmail.com>
2025-07-17 18:13:21 +0200

    Replace contact form with email

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -9,8 +9,7 @@ export const BLUESKY_LINK = "https://bsky.app/profile/swapfest.berlin";
 export const MATRIX_LINK =
   "https://matrix.to/#/!wZyJLpRBZamVkKXskN:matrix.org?via=matrix.org";
 
-export const CONTACT_LINK =
-  "https://cryptpad.fr/form/#/2/form/view/FvacuuCM5hGIaUyfb16dtRXzyPKd+qtsW+Wo9FoUBck/";
+export const CONTACT_LINK = "mailto:swapfest.berlin@proton.me";
 
 export const GITHUB_LINK = "https://github.com/brandonhaslegs/berlin-swapfest";
 
