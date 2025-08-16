02b630f297e476e9790cb5e112124fcc335d394c
Jeff Reiner <theundeadghost@gmail.com>
2025-08-16 14:06:45 +0200

    Adding in new date, stubbing in links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,16 +1,15 @@
-export const REGISTER_LINK =
-  "https://cryptpad.fr/form/#/2/form/view/sH5bEqyLMF9pUJp5autvBgrRWvNWdZ+W5WX85aywHE0/";
+export const REGISTER_LINK = "#!";
 
-export const REGISTER_TABLE_LINK =
-  "https://cryptpad.fr/form/#/2/form/view/givYlhPeujRAMIdzVEXh99kslfEN15l9ejiJ0+2ERHQ/";
+export const REGISTER_TABLE_LINK = "#!";
+
+export const SMOKESIGNAL_LINK = "#!";
 
 export const BLUESKY_LINK = "https://bsky.app/profile/swapfest.berlin";
 
-export const MATRIX_LINK =
-  "https://matrix.to/#/!wZyJLpRBZamVkKXskN:matrix.org?via=matrix.org";
+export const MATRIX_LINK = "https://matrix.to/#/!wZyJLpRBZamVkKXskN:matrix.org";
 
 export const CONTACT_LINK = "mailto:swapfest.berlin@proton.me";
 
-export const GITHUB_LINK = "https://github.com/brandonhaslegs/berlin-swapfest";
+export const GITHUB_LINK = "https://github.com/mirshko/berlin-swapfest";
 
 export const NEWSLETTER_LINK = "https://buttondown.com/swapfest.berlin";
